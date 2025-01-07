09f44c99f0c1e633f1a38a4fdbbaaf9839fa6e40
Alec Sherman <alec.sherman@me.com>
2025-01-07 15:44:04 -0800

    wtkLibrary.js - scroll to top of dashboard when return to home/dashboard

--- app/public/wtk/js/wtkLibrary.js
+++ app/public/wtk/js/wtkLibrary.js
@@ -699,6 +699,15 @@ function pageTransition(fncFrom, fncTo, fncPage = ''){
     if ((fncFrom == 'dashboard') && (fncTo == 'dashboard')){
         fncFrom = 'mainPage';
     }
+    if (fncTo == 'dashboard'){
+        const fncTopOfPage = document.getElementById('myNavbar');
+        if (fncTopOfPage) {
+            setTimeout(function () {
+                fncTopOfPage.scrollIntoView({ behavior: 'smooth' });
+                wtkDebugLog('pageTransition: scroll to myNavbar');
+            }, 360);
+        }
+    }
     if ((fncFrom == 'mainPage') && (fncTo == 'mainPage')){
         pgMainPage = '... loading ...';
         wtkDebugLog('pageTransition: pgMainPage = ' + pgMainPage);
