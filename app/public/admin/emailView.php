62bb0cafa74a970835358331ecbc4ed4ef2e462d
Alec Sherman <alec.sherman@me.com>
2025-02-21 13:15:46 -0800

    improve displaying past-sent emails with HTML templates without breaking main website's CSS

--- app/public/admin/emailView.php
+++ app/public/admin/emailView.php
@@ -5,18 +5,19 @@ if (!isset($gloConnected)):
     require('../wtk/wtkLogin.php');
 endif;
 
-$gloForceRO = wtkPageReadOnlyCheck('/admin/emailView.php', $gloId);
+$pgStandAloneView = wtkGetParam('useHtml');
+$pgModalWin = 'N';
 
-if ($gloRNG != 0): // must be called as Modal Window
-    $pgModalWin = 'Y';
-else:
-    $pgModalWin = 'N';
+if ($pgStandAloneView != 'Y'):
+    if ($gloRNG != 0): // must be called as Modal Window
+        $pgModalWin = 'Y';
+    endif;
 endif;
 
 $pgSQL =<<<SQLVAR
 SELECT e.`UID`, DATE_FORMAT(e.`AddDate`,'$gloSqlDateTime') AS `SentDate`,
     COALESCE(DATE_FORMAT(e.`EmailOpened`, '$gloSqlDateTime'),'not yet') AS `EmailOpened`,
-  COALESCE(t.`EmailCode`, 'none') AS `EmailCode`,
+  COALESCE(t.`EmailCode`, 'none') AS `EmailCode`, e.`HtmlTemplate`,
   CASE
     WHEN e.`SendByUserUID` IS NULL THEN 'Server'
     ELSE CONCAT(u.`FirstName`, ' ', COALESCE(u.`LastName`,''))
@@ -32,11 +33,15 @@ SQLVAR;
 $pgSQL  = wtkSqlPrep($pgSQL);
 wtkSqlGetRow($pgSQL, [$gloId]);
 
-if ($pgModalWin == 'Y'):
-    $pgHtm  = '<div class="row">' . "\n";
-    $pgHtm .= '    <div class="col s12">' . "\n";
+if ($pgStandAloneView == 'Y'):
+    $pgHtm  = '<br><div style="margin-left:50px">' . "\n";
 else:
-    $pgHtm  = '<div class="container">' . "\n";
+    if ($pgModalWin == 'Y'):
+        $pgHtm  = '<div class="row">' . "\n";
+        $pgHtm .= '    <div class="col s12">' . "\n";
+    else:
+        $pgHtm  = '<div class="container">' . "\n";
+    endif;
 endif;
 $pgHtm .=<<<htmVAR
     <h2>Email Detail</h2><br>
@@ -55,39 +60,57 @@ $pgHtm .= wtkFormText('wtkEmailsSent', 'EmailOpened');
 $pgHtm .= wtkFormText('wtkEmailsSent', 'SentTo');
 $pgHtm .= wtkFormText('wtkEmailsSent', 'EmailAddress');
 $pgHtm .= wtkFormText('wtkEmailsSent', 'Subject', 'text', 'Subject', 's12');
-$gloForceRO = false;
 
-$pgEmailBody = wtkSqlValue('EmailBody');
-$pgHtm .=<<<htmVAR
-</div>
-<div class="row">
-    <div class="col s12" style="border:1px">
-        $pgEmailBody
-    </div>
-</div>
-<div class="row">
-htmVAR;
-if ($pgModalWin == 'N'):
+$gloForceRO = wtkPageReadOnlyCheck('/admin/emailView.php', $gloId);
+
+if (($pgModalWin == 'N') && ($pgStandAloneView != 'Y')):
     $pgHtm .= wtkFormTextArea('wtkEmailsSent', 'InternalNote');
 endif;
+
+$pgEmailBody = wtkSqlValue('EmailBody');
+$pgEmailHtmlemplate = wtkSqlValue('HtmlTemplate');
+if ($pgEmailHtmlemplate != ''):
+    if ($pgStandAloneView == 'Y'):
+        $pgHtmlTemplate = wtkLoadInclude(_WTK_RootPATH . 'htm/' . $pgEmailHtmlemplate);
+        $pgHtmlTemplate = wtkTokenToValue($pgHtmlTemplate);
+        $pgEmailBody = wtkReplace($pgHtmlTemplate,'@wtkContent@',$pgEmailBody);
+    else:
+        $pgTmp  = '<a target="_blank" class="btn" href="emailView.php?id=' . $gloId . '&useHtml=Y&apiKey=' . $pgApiKey . '">' . "\n";
+        $pgTmp .= 'View as seen with html framework</a><br><hr>' . "\n";
+        $pgEmailBody = $pgTmp . $pgEmailBody;
+    endif;
+endif;
+$pgHtm .=<<<htmVAR
+    </div>
+    <div class="row">
+        <div class="col s12" style="border:1px">
+            $pgEmailBody
+        </div>
+    </div>
+htmVAR;
+
 $pgHtm .= wtkFormHidden('ID1', $gloId);
 $pgHtm .= wtkFormHidden('UID', wtkEncode('UID'));
+//$pgHtm .= wtkFormPrimeField('wtkEmailsSent', 'ParentUID', $gloRNG);
 $pgHtm .= wtkFormHidden('wtkMode', $gloWTKmode);
 $pgHtm .= wtkFormHidden('wtkGoToURL', '../../admin/emailHistory.php');
-//$pgHtm .= wtkFormPrimeField('wtkEmailsSent', 'ParentUID', $gloRNG);
-$pgHtm .= '            </div>' . "\n";
-if ($pgModalWin == 'Y'):
-    $pgHtm .= '<div class="center"><button type="button" class="btn-small black b-shadow waves-effect waves-light modal-close">Close</button></div>';
-else:
-    $pgHtm .= wtkUpdateBtns() . "\n";
-endif;
-$pgHtm .= wtkFormWriteUpdField();
-
 $pgHtm .=<<<htmVAR
         </form>
-    </div>
-</div>
 htmVAR;
+if ($pgStandAloneView != 'Y'):
+    if ($pgModalWin == 'Y'):
+        $pgHtm .= '<div class="center"><button type="button" class="btn-small black b-shadow waves-effect waves-light modal-close">Close</button></div>';
+    else:
+        $pgHtm .= wtkUpdateBtns() . "\n";
+    endif;
+    $pgHtm .= wtkFormWriteUpdField();
+endif;
+if ($pgStandAloneView != 'Y'):
+    $pgHtm .= '    <br>' . "\n";
+endif;
+$pgHtm .= '    </div>' . "\n";
+$pgHtm .= '</div>' . "\n";
+
 if ($pgModalWin == 'Y'):
     $pgHtm .= '</div>' . "\n";
 endif;
